106940a3e9f265e3d7e4aed3fd64c3e1e9ba084e
JannatunNimeNishat <nishatnime100@gmail.com>
2023-06-07 12:45:17 +0600

    api - create jwt token send back to the client api added

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const app = express()
 const port = process.env.port || 5000;
 
 //middle wares
-app.get(cors())
-app.get(express.json())
-
+app.use(cors())
+app.use(express.json())
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 //JWT
-
+app.post('/jwt', (req,res)=>{
+  
+    const email = req.body;
+   const token = jwt.sign(email,process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+    res.send({token})
+})
 
 
 app.get('/', (req,res)=>{
